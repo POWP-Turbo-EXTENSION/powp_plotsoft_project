f2cf7b5f9da9693367980487ce7cff92adcff6e5
Nerond32 <nerond32@gmail.com>
2017-06-05 13:59:36 +0200

    [GUI-dodatek] dodanie toolbara z pozycja 'File'

--- src/main/java/edu/iis/powp/command/gui/ComplexCommandEditorWindow.java
+++ src/main/java/edu/iis/powp/command/gui/ComplexCommandEditorWindow.java
@@ -3,16 +3,31 @@ package edu.iis.powp.command.gui;
 import java.awt.Container;
 import java.awt.GridBagLayout;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
 import edu.iis.powp.command.manager.PlotterCommandManager;
 
 public class ComplexCommandEditorWindow extends JFrame {
 	private PlotterCommandManager commandManager;
-
+	private JMenuBar menuBar;
+	private JMenu menuFile;
+	
 	public ComplexCommandEditorWindow(PlotterCommandManager commandManager) {
 		this.setTitle("Command Editor");
 		this.setSize(400, 400);
 		Container content = this.getContentPane();
 		content.setLayout(new GridBagLayout());
 		this.commandManager = commandManager;
+		InitMenu();
+	}
+	
+	private void InitMenu(){
+		menuBar = new JMenuBar();
+		menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		this.setJMenuBar(menuBar);
+		this.validate();
+		this.repaint();
 	}
 }
