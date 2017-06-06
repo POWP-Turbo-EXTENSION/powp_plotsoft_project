9449db17ea70877883dfb08a7d5cc9312600c05f
Piotr Treliński <195078@edu.p.lodz.pl>
2017-06-06 20:45:33 +0200

    suicidal thoughts intensify

--- src/main/java/edu/iis/powp/command/gui/CommandEditorWindow.java
+++ src/main/java/edu/iis/powp/command/gui/CommandEditorWindow.java
@@ -1,13 +1,19 @@
 package edu.iis.powp.command.gui;
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import edu.iis.powp.command.manager.PlotterCommandManager;
 import edu.iis.powp.observer.Subscriber;
@@ -20,6 +26,7 @@ public class CommandEditorWindow extends JFrame implements WindowComponent {
 	private JTextArea observerListField;
 	private JMenuBar menuBar;
 	private JMenu file;
+	private JSplitPane window;
 	
 	private static final long serialVersionUID = 9204679248304669948L;
 	
@@ -29,19 +36,23 @@ public class CommandEditorWindow extends JFrame implements WindowComponent {
 		menuBar = new JMenuBar();
 		this.setJMenuBar(menuBar);
 		file = new JMenu("File");
+		file.add(new JMenuItem("New"));
+		file.add(new JMenuItem("Open"));
+		file.add(new JMenuItem("Save"));
 		menuBar.add(file);
 		Container content = this.getContentPane();
-		GridBagLayout mainGridBagLayout = new GridBagLayout();
-		mainGridBagLayout.columnWidths = new int[] { 0, 540 };
-		mainGridBagLayout.rowHeights = new int[] { 0, 0 };
-		mainGridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		mainGridBagLayout.rowWeights = new double[] { 0.0, 1.0 };
-		content.setLayout(mainGridBagLayout);
+		content.setLayout(new BorderLayout());
 		this.commandManager = commandManager;
 		observerListField = new JTextArea("");
 		observerListField.setEditable(false);
 		updateObserverListField();
-
+		JPanel top = new JPanel();
+        JPanel bottom = new JPanel();
+        JSplitPane bar = new JSplitPane(JSplitPane.VERTICAL_SPLIT, top, bottom);
+        JPanel drawSpace = new JPanel();
+        window = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, bar, drawSpace);
+        content.add(window, BorderLayout.CENTER);
+		
 
 	}
 	
