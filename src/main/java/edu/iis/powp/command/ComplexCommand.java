92830d29891c040e760146fc196ea89e05f979af
Piotr Treliński <195078@edu.p.lodz.pl>
2017-06-18 17:12:23 +0200

    Added serialization code to complex command by hand

--- src/main/java/edu/iis/powp/command/ComplexCommand.java
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -1,21 +1,36 @@
 package edu.iis.powp.command;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.StreamCorruptedException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import javax.swing.JFileChooser;
+
 import edu.iis.client.plottermagic.IPlotter;
 import edu.iis.powp.command.visitor.Visitor;
 
-public class ComplexCommand implements ICompoundCommand {
+public class ComplexCommand implements ICompoundCommand, Serializable {
 
 	List<IPlotterCommand> commands = new ArrayList<>();
+	private String name = null;
 
-	public ComplexCommand() {
-		super();
-	}
-
+    public ComplexCommand() {
+        super();
+    }
+    
+    public ComplexCommand(String name){
+    	super();
+    	Load(name);
+}
 	@Override
 	public void execute(IPlotter plotter) {
 		for (Iterator iterator = commands.iterator(); iterator.hasNext();) {
@@ -53,5 +68,80 @@ public class ComplexCommand implements ICompoundCommand {
 			tmp.commands.add(ipc);
 		}
 		return tmp;
+	}
+	public void Save(String name){
+	      try {
+	          FileOutputStream fileOut = new FileOutputStream("./src/commands/" + name + ".ser");
+	          ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	          out.writeObject(commands);
+	          out.close();
+	          fileOut.close();
+	          System.out.printf("Serialized command at " + "/commands/" + name + ".ser");
+	       }catch(IOException i) {
+	          i.printStackTrace();
+	       }
+		}
+		public void Save(){
+		    if(name!=null){
+				try {
+			        FileOutputStream fileOut = new FileOutputStream("./src/commands/" + name + ".ser");
+			        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			        out.writeObject(commands);
+			        out.close();
+			        fileOut.close();
+			        System.out.printf("Serialized command at " + "/commands/" + name + ".ser");
+				}catch(IOException i) {
+					i.printStackTrace();
+			    }
+		    }
+		}
+		public void Load(String name){
+		      try {
+		    	  this.name = name;
+		          FileInputStream fileIn = new FileInputStream("./src/commands/" + name + ".ser");
+		          ObjectInputStream in = new ObjectInputStream(fileIn);
+		          commands = (List<IPlotterCommand>) in.readObject();
+		          in.close();
+		          fileIn.close();
+		       }catch(IOException i) {
+		          i.printStackTrace();
+		       }catch(ClassNotFoundException c) {
+		          System.out.println("Employee class not found");
+		          c.printStackTrace();
+		       }
+		}
+		public void Load(){
+		      try {
+		    	  JFileChooser fileChooser = new JFileChooser("./src/commands/");
+		    	  int result = fileChooser.showOpenDialog(null);
+		    	  if (result == JFileChooser.APPROVE_OPTION) {
+			          FileInputStream fileIn = new FileInputStream(fileChooser.getSelectedFile().getAbsolutePath());
+			          name = fileChooser.getSelectedFile().getName();
+			          name = name.substring(0, name.length()-4);
+			          ObjectInputStream in = new ObjectInputStream(fileIn);
+			          commands = (List<IPlotterCommand>) in.readObject();
+			          in.close();
+			          fileIn.close();
+		    	  }
+		       }catch(StreamCorruptedException s){
+		    	   System.out.println("Chosen file is not a command list");
+		       }catch(IOException i) {
+		          i.printStackTrace();
+		       }catch(ClassNotFoundException c) {
+		          System.out.println("Class not found");
+		          c.printStackTrace();
+		       }
+		}
+		public static List<ComplexCommand> findComplexCommands(){
+			List<ComplexCommand> list = new ArrayList<ComplexCommand>();
+			
+			File folder = new File("./src/commands/");
+			File[] listOfFiles = folder.listFiles();
+			for (File file : listOfFiles) {
+			    if (file.isFile()) {
+			        list.add(new ComplexCommand(file.getName().substring(0, file.getName().length()-4)));
+			    }
+			}
+			return list;
 	}
 }
